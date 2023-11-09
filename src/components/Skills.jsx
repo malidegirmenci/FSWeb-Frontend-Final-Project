32fb3ea54a2af9d19a116ca3994d3360fb9dc36f
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-09 22:50:20 +0300

    changed to grid template

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,15 +4,15 @@ export default function Skills() {
     const { skills,headers } = useSelector((state) => state.data);
     const language = useSelector((state) => state.language);
     return (
-        <div className='flex w-[70%]  py-[6rem]  dark:text-white first-letter: mx-auto gap-16 justify-between'>
-            <h2 className=' w-[33%] text-indigo-700 dark:text-lime-300 text-5xl font-bold tracking-wide'>{language === "ENG" ? headers.skills.ENG : headers.skills.TR}</h2>
-            <div className='w-[67%] flex gap-10 flex-wrap justify-between'>
+        <div className='grid grid-cols-[40%,60%] w-[70%]  py-[6rem] max-lg:grid-cols-1 max-lg:gap-10 dark:text-white first-letter: mx-auto'>
+            <h2 className=' text-indigo-700 dark:text-lime-300 text-5xl font-bold tracking-wide'>{language === "ENG" ? headers.skills.ENG : headers.skills.TR}</h2>
+            <div className='grid grid-cols-2 max-md:grid-cols-1 gap-10'>
                 {
                     skills.map((item, index) => {
                         return (
-                            <div className='flex gap-6 items-center w-[35%] ' key={index}>
+                            <div className='grid grid-cols-2 items-center gap-2 ' key={index}>
                                 <img  src={item.img} alt={`${item.name}icon`} />
-                                <h3 className=' text-neutral-500 dark:text-white text-2xl font-medium uppercase leading-9'>{item.name}</h3>
+                                <h3 className=' text-neutral-500 dark:text-white text-2xl max-md:text-xl font-medium uppercase leading-9'>{item.name}</h3>
                             </div>
                         )
                     })
