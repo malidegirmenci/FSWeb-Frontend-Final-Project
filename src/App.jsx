5783854b7ff56e1cba1c7b40d1187a7eaeba605c
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-11-10 16:09:56 +0300

    dark mode toggle hot fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Skills from './components/Skills'
 import { fetchAnother } from './store/actions'
 import { useDispatch, useSelector } from 'react-redux'
 import { RiseLoader } from "react-spinners";
-import { DarkModeContext } from './context/darkModeContext';
+import { DarkModeContext } from './context/darkModeContext.jsx';
 
 
 function App() {
